31c76d17a26f727b742241ac461d966338581457
JuaanJoH <jjhv2111github@gmail.com>
2024-11-16 22:32:00 -0500

    eliminacion 2 usuarios

--- start/routes/users.ts
+++
@@ -1,9 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-Route.group(() => {
-    Route.get("/users", "UsersController.find");
-    Route.get("/users/:id", "UsersController.find");
-    Route.post("/users", "UsersController.create");
-    Route.put("/users/:id", "UsersController.update");
-    Route.delete("/users/:id", "UsersController.delete");
-})
-//.middleware(['security']) //quitar en caso de que se quiera acceder sin necesidad de la autentificacion
