e7e159cb065a0d099a54dd87d4a308b0245e4d55
JuaanJoH <jjhv2111github@gmail.com>
2024-11-17 21:35:42 -0500

    Implementacion ms notificaciones-administrator

--- app/Controllers/Http/AdministratorsController.ts
+++ app/Controllers/Http/AdministratorsController.ts
@@ -48,42 +48,61 @@ export default class AdministratorsController {
     }
   }
 
-  public async create({ request, response }: HttpContextContract) {
-    try {
-      // Validar datos usando el administratorValidator
-      const body = request.body();
-
-      // Llamada al microservicio de usuarios
-      const theUserResponse = await axios.get(
-        `${Env.get("MS_SECURITY")}/users/${body.user_id}`,
-        {
-          headers: { Authorization: request.headers().authorization || "" },
+    // Definimos el metodo que recibe un objeto con una request http y una respuesta 
+    public async create({ request, response }: HttpContextContract) { 
+      try {
+        //Extrae el cuerpo de la solicitud http, que contiene los datos del nuevo conductor
+        const body = request.body(); 
+  
+        //Validar datos del usuario en el microservicio de seguridad
+        const theUserResponse = await axios.get( // Realiza una solicitud get al microservicio de seguridad para verificar si existe el usuario asociado al user_id
+          `${Env.get("MS_SECURITY")}/users/${body.user_id}`, // Construye la URL usando la variable de entorno MS_SECURITY y el user_id del cuerpo de la solicitud
+          {
+            // Incluye el token de autorización, si está disponible, en los encabezados de la solicitud
+            headers: { Authorization: request.headers().authorization || "" }, 
+          }
+        );
+  
+        // Verifica si no se encontró información del usuario en la respuesta del microservicio
+        // Devuelve una respuesta 404 si el usuario no fue encontrado
+        if (!theUserResponse.data || Object.keys(theUserResponse.data).length === 0) { 
+          return response.notFound({ 
+            error: "User not found, verify user_id", 
+          });
         }
-      );
-      
-      // Verificar si no se encontró información del usuario en el microservicio
-      if (!theUserResponse.data || Object.keys(theUserResponse.data).length === 0) {
-        return response.notFound({
-          error:
-            "User not found, verify user_id",
-        });
+  
+        // Crear el conductor.
+        await request.validate(AdministratorValidator); // Validador
+        const theAdministrator = await Administrator.create(body); // Crea un nuevo registro de conductor en la bd
+  
+        // Enviar correo de bienvenida
+        // Realiza una solicitud POST (Ya que vamos a enviar un cuerpo) al ms de notificaciones
+        const welcomeEmailResponse = await axios.post( 
+          `${Env.get("MS_NOTIFICATIONS")}/send-welcome-email`, // Construye la URL del endpoint del ms 
+          {
+            //Envia el nombre y el correo del usuario obtenido desde el ms de seguridad
+            name: theUserResponse.data.name, 
+            email: theUserResponse.data.email, 
+          }
+        );
+          // Verifica si la respuesta del ms de notificaciones no es exitosa y imprime el error
+        if (welcomeEmailResponse.status !== 200) { 
+          console.error("Error enviando el correo de bienvenida:", welcomeEmailResponse.data); 
+        }
+        
+        // Devuelve el conductor
+        return theAdministrator;  
+        
+      } catch (error) {
+        if (error.messages) { // Si el error tiene mensajes de validación, devuelve esos mensajes al cliente.
+          return response.badRequest({ errors: error.messages.errors }); // Devuelve una respuesta 400 (Solicitud inválida) con los errores de validación.
+        }
+        throw new Exception( // Si ocurre cualquier otro error, lanza una excepción genérica.
+          error.message || "Error al procesar la solicitud", // Mensaje del error.
+          error.status || 500 // Código de estado HTTP del error.
+        );
       }
-      // Crear el administrator si la validación y la verificación de usuario son exitosas
-      await request.validate(AdministratorValidator);
-      const theAdministrator: Administrator = await Administrator.create(body);
-      return theAdministrator;
-    } catch (error) {
-      // Si el error es de validación, devolver los mensajes de error de forma legible
-      if (error.messages) {
-        return response.badRequest({ errors: error.messages.errors });
-      }
-      // Para cualquier otro tipo de error, lanzar una excepción genérica
-      throw new Exception(
-        error.message || "Error al procesar la solicitud",
-        error.status || 500
-      );
     }
-  }
 
   public async update({ params, request }: HttpContextContract) {
     const theAdministrator: Administrator = await Administrator.findOrFail(params.id); //busque el teatro con el identificador
