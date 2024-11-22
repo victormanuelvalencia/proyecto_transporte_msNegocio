8be9094be49a643fdddc38d26bdd5b70d64e943d
victormanuelvalencia <vvalenciaa.1@icloud.com>
2024-11-22 01:18:02 -0500

    Conflictos resuletos

--- app/Validators/AdministratorValidator.ts
+++ app/Validators/AdministratorValidator.ts
@@ -1,4 +1,4 @@
-import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator';
+import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator';
 
 export default class AdministratorValidator {
   public schema = schema.create({/*
