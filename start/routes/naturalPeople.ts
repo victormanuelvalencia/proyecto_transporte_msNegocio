1c62cb200d90f16f2ab49362e3121f8629323eb8
victormanuelvalencia <vvalenciaa.1@icloud.com>
2024-12-10 23:06:28 -0500

    Corrección rutas

--- start/routes/naturalPeople.ts
+++
@@ -1,8 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-Route.group(() => {
-    Route.get("/NaturalPeople", "NaturalPersonController.find");
-    Route.get("/NaturalPeople/:id", "NaturalPersonController.find");
-    Route.post("/NaturalPeople", "NaturalPersonController.create");
-    Route.put("/NaturalPeople/:id", "NaturalPersonController.update");
-    Route.delete("/NaturalPeople/:id", "NaturalPersonController.delete");
-})
